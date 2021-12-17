d29434a9b8bcf4b5ea101fe9542cd96bfe5f5e04
Andrija Gajic <andrijagajicbusiness@gmail.com>
2021-12-17 15:26:42 +0100

    chore: add image and name of every state

--- src/code.js
+++ src/code.js
@@ -1,4 +1,52 @@
-document.getElementById('select-location').addEventListener('change', function() {
-    const grad = this.value;
-    document.querySelector('iframe').src = `https://naslovi.net/vremenska-prognoza/${grad}#w-next`;
-});
+var zvukTrenutnoPusten = false;
+
+const zivotinjeInformacije = [{
+        lokacijaAudioFajla: './animal_sounds/cow-moo.wav',
+        opisZivotinje: 'Krava je domaca zivotinja braon-bele ili crno-bele boje i daje mleko'
+    },
+    {
+        lokacijaAudioFajla: './animal_sounds/cat-meow.wav',
+        opisZivotinje: 'Macka je mala zivotinja, lovi miseve i pije mleko'
+    },
+    {
+        lokacijaAudioFajla: './animal_sounds/dog-sound.wav',
+        opisZivotinje: 'Pas laje, voli da se igra, covekov najbolji prijatelj'
+    },
+    {
+        lokacijaAudioFajla: './animal_sounds/bear-sound.mp3',
+        opisZivotinje: 'Medved je veliki, jede mnogo, a popularan je ljubimac u Rusiji',
+    },
+    {
+        lokacijaAudioFajla: './animal_sounds/horse-sound.wav',
+        opisZivotinje: 'Konj je plemenita zivotinja, jak',
+    },
+    {
+        lokacijaAudioFajla: './animal_sounds/rooster-sound.mp3',
+        opisZivotinje: 'Pevac kukurice rano ujutru, voli da zaokupi decu'
+    }
+];
+
+// const images = document.querySelectorAll('img');
+// images.forEach((image, idx) => {
+//     // za svaku sliku pusti audio kad se predje misom
+//     image.addEventListener('mouseover', () => {
+//         playAudio(zivotinjeInformacije[idx].lokacijaAudioFajla);
+//     });
+
+//     // za klik izbaci opis
+//     image.addEventListener('click', () => {
+//         alert(zivotinjeInformacije[idx].opisZivotinje);
+//     });
+// });
+
+// function playAudio(audioPath) {
+//     // proveri da li se trenutno emituje neki zvuk
+//     if (zvukTrenutnoPusten === false) {
+//         const audio = new Audio(audioPath);
+//         audio.play();
+//         zvukTrenutnoPusten = true;
+//         audio.onended = () => {
+//             zvukTrenutnoPusten = false;
+//         };
+//     }
+// }
